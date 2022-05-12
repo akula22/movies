edc2ec83e995dd0275eb76880c66798b54865577
Vladimir Akulov <support@opipo.ru>
2022-05-13 02:51:22 +0300

    lesson-1

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.movies.navigation.SetupNavHost
 import com.example.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoviesTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/movies/navigation/SetupNavHost.kt
@@ -0,0 +1,35 @@
+package com.example.movies.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.movies.screens.MainScreen
+import com.example.movies.screens.SplashScreen
+import com.example.movies.utils.Constants
+
+sealed class Screens(val route: String) {
+    object Splash : Screens(route = Constants.Screens.SPLASH_SCREEN)
+    object Main : Screens(route = Constants.Screens.MAIN_SCREEN)
+    object Detail : Screens(route = Constants.Screens.DETAIL_SCREEN)
+}
+
+@Composable
+fun SetupNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Splash.route
+    ) {
+
+        composable(route = Screens.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(route = Screens.Main.route) {
+            MainScreen()
+        }
+        composable(route = Screens.Detail.route) {
+
+        }
+    }
+}
